1c2452fb45afdf863158fca289525f79ba3f3b03
Alex <alex@abarthel.me>
2024-04-19 15:58:53 +0200

    add plugin info

--- TF2_64bit.cs/TF2_64bit.cs
+++ TF2_64bit.cs/TF2_64bit.cs
@@ -5,12 +5,23 @@ using WindowsGSM.GameServer.Engine;
 namespace WindowsGSM.Plugins
 {
     public class TF2_64bit : Source
-    {
+    {        
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.TF2_64bit",
+            author = "Raziel7893",
+            description = "WindowsGSM plugin for supporting TF2 using 64bit Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/Raziel7893/WindowsGSM.TF2_64bit",
+            color = "#34FFeb"
+        };
         // - Plugin Details
         public string FullName = "Team Fortress 2 64bit Dedicated Server";
         public override string Defaultmap { get { return "cp_badlands"; } }
         public override string Game { get { return "tf"; } }
         public override string AppId { get { return "232250"; } }
+        public override string Additional { get { return "-tickrate 64"; } }
 
         public string StartPath = "srcds_win64.exe";
 
@@ -18,6 +29,5 @@ namespace WindowsGSM.Plugins
         {
             base.serverData = serverData;
         }
-
     }
 }
